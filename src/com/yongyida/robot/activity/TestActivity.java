d6901f7314e47d8a7e4a08f44b5424adefd0f278
lilei <lilei@yongyida.com>
2017-01-03 14:44:08 +0800

    添加 新功能

---
+++ src/com/yongyida/robot/activity/TestActivity.java
@@ -0,0 +1,17 @@
+package com.yongyida.robot.activity;
+
+import android.os.Bundle;
+
+import com.yongyida.robot.R;
+
+/**
+ * Created by Administrator on 2016/12/22 0022.
+ */
+
+public class TestActivity extends OriginalActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
